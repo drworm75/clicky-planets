a9550d371c6417c43ee956ccfd91d0c316e9e47f
drworm75 <dwgames@gmail.com>
2017-02-21 21:34:35 -0600

    planet names apper on click

--- script.js
+++ script.js
@@ -1,38 +1,39 @@
 var planetBtn = document.getElementById("showPlanets");
 var planetCards = document.getElementById("planetHolder");
 
+
 var planets = [
     {
         name:"Mercury",
-        url: ""
+        url: "https://www.nasa.gov/sites/default/files/mercury_1.jpg"
     },
     {
         name:"Venus",
-        url: ""
+        url: "http://cdn.nextgov.com/media/img/upload/2014/12/23/122314venusNG/nextgov-medium.jpg"
     },
     {
         name:"Earth",
-        url: ""
+        url: "https://www.nasa.gov/centers/goddard/images/content/638831main_globe_east_2048.jpg"
     },
     {
         name:"Mars",
-        url: ""
+        url: "https://www.nasa.gov/sites/default/files/thumbnails/image/mars_2.jpg"
     },
     {
         name:"Jupiter",
-        url: ""
+        url: "https://www.nasa.gov/sites/default/files/jupiter_1_0.jpg"
     },
     {
         name:"Saturn",
-        url: ""
+        url: "http://photojournal.jpl.nasa.gov/jpeg/PIA01364.jpg"
     },
     {
         name:"Uranus",
-        url: ""
+        url: "https://spaceplace.nasa.gov/review/all-about-uranus/uranus2.en.jpg"
     },
     {
         name:"Neptune",
-        url: ""
+        url: "https://www.nasa.gov/sites/default/files/thumbnails/imageneptune_full.jpg"
     }
 ];
 
@@ -40,10 +41,25 @@ function writeToDom() {
 	planetCards.innerHTML = "";
 	for (var i = 0; i < planets.length; i++) {
 		var planetList = "";
-		planetList += `<div class="planetBox", id="planetBox-${i+1}">`;
-	  	planetList += `<div class="planetName"> ${planets[i].name} <br>`;
+		planetList += `<div class="planetBox", id="planetBox-${i}">`;
+	  	planetList += `<div class="planetName hidden"> ${planets[i].name}`;
 	  	planetList += `</div>`;
+	  	planetList += `<img class="planetImage" src="${planets[i].url}">`
 		planetCards.innerHTML += planetList;
 	}
 }
-planetBtn.addEventListener("click",writeToDom);
+
+function showMeTheMoney(event) {
+	if(event.target.className === 'planetImage') {
+		console.log("event worked", event);
+		console.log("uniqueID", event.target.parentNode.id);
+		console.log("text?", event.target.previousSibling); //console the event out
+		event.target.previousSibling.classList.remove("hidden");
+	}
+}
+
+planetBtn.addEventListener("mouseenter", writeToDom);
+document.body.addEventListener("click", showMeTheMoney);
+
+//Make planet names disapper
+//Make planets disapper (Maybe with a click event?)
